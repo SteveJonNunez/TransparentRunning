d106e8cb46829d27a584b1ec2682680d6bfd9df5
SteveNunez <runningtechy@gmail.com>
2020-04-22 00:32:10 -0400

    Added room database converters

---
+++ app/src/main/java/com/runningtechie/transparentrunning/TransparentRunningConverters.kt
@@ -0,0 +1,16 @@
+package com.runningtechie.transparentrunning
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class TransparentRunningConverters {
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun toDate(millisecondSinceEpoch: Long?): Date? {
+        return millisecondSinceEpoch?.let { Date(it) }
+    }
+}
--- app/src/main/java/com/runningtechie/transparentrunning/TransparentRunningDatabase.kt
+++ app/src/main/java/com/runningtechie/transparentrunning/TransparentRunningDatabase.kt
@@ -2,8 +2,10 @@ package com.runningtechie.transparentrunning
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.runningtechie.transparentrunning.enitity.LocationPoint
 import com.runningtechie.transparentrunning.enitity.WorkoutSession
 
 @Database(entities = [WorkoutSession::class, LocationPoint::class], version = 1)
+@TypeConverters(TransparentRunningConverters::class)
 abstract class TransparentRunningDatabase : RoomDatabase()
