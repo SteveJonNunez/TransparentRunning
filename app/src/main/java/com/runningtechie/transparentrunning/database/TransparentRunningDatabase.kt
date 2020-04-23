d53cf265d4daedf72191156b004eda012d5bb0c8
SteveNunez <runningtechy@gmail.com>
2020-04-22 20:33:01 -0400

    Added dao to the room database

--- app/src/main/java/com/runningtechie/transparentrunning/database/TransparentRunningDatabase.kt
+++ app/src/main/java/com/runningtechie/transparentrunning/database/TransparentRunningDatabase.kt
@@ -4,9 +4,17 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.runningtechie.transparentrunning.database.TransparentRunningConverters
+import com.runningtechie.transparentrunning.database.dao.LocationPointDao
+import com.runningtechie.transparentrunning.database.dao.WorkoutSessionDao
+import com.runningtechie.transparentrunning.database.dao.WorkoutSessionWithLocationPointsDao
 import com.runningtechie.transparentrunning.model.LocationPoint
 import com.runningtechie.transparentrunning.model.WorkoutSession
 
 @Database(entities = [WorkoutSession::class, LocationPoint::class], version = 1)
 @TypeConverters(TransparentRunningConverters::class)
-abstract class TransparentRunningDatabase : RoomDatabase()
+abstract class TransparentRunningDatabase : RoomDatabase() {
+
+    abstract fun workoutSessionDao(): WorkoutSessionDao
+    abstract fun locationPointDao(): LocationPointDao
+    abstract fun workoutSessionWithLocationPointsDao(): WorkoutSessionWithLocationPointsDao
+}
