6b9a6c66f29defebb5bb7f9e99560d548e05fa14
SteveNunez <runningtechy@gmail.com>
2020-04-23 23:27:04 -0400

    added an Application class

---
+++ app/src/main/java/com/runningtechie/transparentrunning/TransparentRunningApplication.kt
@@ -0,0 +1,10 @@
+package com.runningtechie.transparentrunning
+
+import android.app.Application
+
+class TransparentRunningApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
