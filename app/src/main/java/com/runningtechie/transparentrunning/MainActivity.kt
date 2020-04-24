f7b9d8eb0045d3b621f291b51e8d41eaf1785a01
SteveNunez <runningtechy@gmail.com>
2020-04-24 01:09:30 -0400

    Added start and stop button for gpsForegroundService

--- app/src/main/java/com/runningtechie/transparentrunning/MainActivity.kt
+++ app/src/main/java/com/runningtechie/transparentrunning/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.runningtechie.transparentrunning
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startButton: Button
+    private lateinit var stopButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.start_button)
+        stopButton = findViewById(R.id.stop_button)
+
+
+        startButton.setOnClickListener {
+            intent = Intent(this, GPSForegroundService::class.java)
+            intent.action = ACTION_START_GPS_FOREGROUND_SERVICE
+            ContextCompat.startForegroundService(this, intent)
+        }
+
+        stopButton.setOnClickListener {
+            intent = Intent(this, GPSForegroundService::class.java)
+            intent.action = ACTION_STOP_GPS_FOREGROUND_SERVICE
+            ContextCompat.startForegroundService(this, intent)
+        }
     }
 }
+
