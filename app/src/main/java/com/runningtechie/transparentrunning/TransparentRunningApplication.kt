caf42f3475894299c9610e0f63908e51df84b81d
SteveNunez <runningtechy@gmail.com>
2020-04-28 22:02:39 -0400

    Created a repository to access the database

--- app/src/main/java/com/runningtechie/transparentrunning/TransparentRunningApplication.kt
+++ app/src/main/java/com/runningtechie/transparentrunning/TransparentRunningApplication.kt
@@ -1,10 +1,12 @@
 package com.runningtechie.transparentrunning
 
 import android.app.Application
+import com.runningtechie.transparentrunning.database.TransparentRunningRepository
 
 class TransparentRunningApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
+        TransparentRunningRepository.initialize(this)
     }
 }
---
+++ app/src/main/java/com/runningtechie/transparentrunning/database/TransparentRunningRepository.kt
@@ -0,0 +1,41 @@
+package com.runningtechie.transparentrunning.database
+
+import android.content.Context
+import androidx.room.Room
+import com.runningtechie.transparentrunning.database.dao.LocationPointDao
+import com.runningtechie.transparentrunning.database.dao.WorkoutSessionDao
+import com.runningtechie.transparentrunning.database.dao.WorkoutSessionWithLocationPointsDao
+import com.runningtechie.transparentrunning.model.WorkoutSession
+
+private const val DATABASE_NAME = "transparent-running-database"
+
+class TransparentRunningRepository private constructor(context: Context) {
+
+    private val database : TransparentRunningDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        TransparentRunningDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    private val workoutSessionDao = database.workoutSessionDao()
+    private val locationPointDao = database.locationPointDao()
+    private val workoutSessionWithLocationPointsDao = database.workoutSessionWithLocationPointsDao()
+
+    fun insertWorkoutSession(workoutSession: WorkoutSession): Long = workoutSessionDao.insert(workoutSession)
+    fun getWorkoutSession(): List<WorkoutSession> = workoutSessionDao.getAllWorkoutSessions()
+
+    companion object {
+        private var INSTANCE: TransparentRunningRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = TransparentRunningRepository(context)
+            }
+        }
+
+        fun get(): TransparentRunningRepository {
+            return INSTANCE
+                ?: throw IllegalStateException("TransparentRunningRepository must be initialized")
+        }
+    }
+}
