c9536de7695de67b96d8a9d100dc02945fa32d9e
SteveNunez <runningtechy@gmail.com>
2020-05-03 01:04:25 -0400

    created class to handle permissions

---
+++ app/src/main/java/com/runningtechie/transparentrunning/PermissionTool.kt
@@ -0,0 +1,20 @@
+package com.runningtechie.transparentrunning
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+class PermissionTool {
+    companion object {
+        const val FINE_LOCATION_REQUEST_CODE: Int = 16
+
+        fun hasFineLocation(activity: Activity): Boolean =
+            (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+
+        fun requestFineLocation(activity: Activity) {
+            ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), FINE_LOCATION_REQUEST_CODE)
+        }
+    }
+}
