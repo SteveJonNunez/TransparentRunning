5005614c12e0d5142c224de230331e2246bfb6dc
SteveNunez <runningtechy@gmail.com>
2020-05-10 02:30:27 -0400

    removed private constructor for utility class

--- app/src/main/java/com/runningtechie/transparentrunning/PermissionTool.kt
+++ app/src/main/java/com/runningtechie/transparentrunning/PermissionTool.kt
@@ -6,7 +6,7 @@ import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
-class PermissionTool {
+class PermissionTool private constructor() {
     companion object {
         const val FINE_LOCATION_REQUEST_CODE: Int = 16
 
