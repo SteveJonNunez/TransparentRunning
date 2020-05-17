592026d4ddbe61e40c411ee85d5b13d0a076ff23
SteveNunez <runningtechy@gmail.com>
2020-05-17 01:30:41 -0400

    Added Timber

--- app/src/main/java/com/runningtechie/transparentrunning/TransparentRunningApplication.kt
+++ app/src/main/java/com/runningtechie/transparentrunning/TransparentRunningApplication.kt
@@ -2,11 +2,17 @@ package com.runningtechie.transparentrunning
 
 import android.app.Application
 import com.runningtechie.transparentrunning.database.TransparentRunningRepository
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
 
 class TransparentRunningApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
         TransparentRunningRepository.initialize(this)
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
     }
 }
