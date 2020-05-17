e612ef614fa7e8505b33b02827908fdf6224810b
SteveNunez <runningtechy@gmail.com>
2020-05-17 19:54:54 -0400

    Fixed query

--- app/src/main/java/com/runningtechie/transparentrunning/database/dao/LocationPointDao.kt
+++ app/src/main/java/com/runningtechie/transparentrunning/database/dao/LocationPointDao.kt
@@ -6,10 +6,10 @@ import androidx.room.Query
 import com.runningtechie.transparentrunning.model.LocationPoint
 
 @Dao
-interface LocationPointDao: BaseDao<LocationPoint> {
+interface LocationPointDao : BaseDao<LocationPoint> {
     @Query("SELECT * from LocationPoint")
     fun getAllLocationPoints(): LiveData<List<LocationPoint>>
 
-    @Query("SELECT * from LocationPoint WHERE sessionId=(:sessionId)")
+    @Query("SELECT * from LocationPoint WHERE sessionId=(:sessionId) ORDER BY time ASC")
     fun getAllLocationPoints(sessionId: Long): LiveData<List<LocationPoint>>
 }
