dd63faecf5027eff686996f40f028adcd717046a
SteveNunez <runningtechy@gmail.com>
2020-06-03 19:08:18 -0400

    TODO

--- app/src/main/java/com/runningtechie/transparentrunning/model/Distance.kt
+++ app/src/main/java/com/runningtechie/transparentrunning/model/Distance.kt
@@ -1,6 +1,7 @@
 package com.runningtechie.transparentrunning.model
 
 class Distance(val meters: Float) {
+    //TODO: Calculate only when needed
     val feet = meters/0.3048f
     val miles = meters*0.000621371f
     val kilometers = meters*.001f
